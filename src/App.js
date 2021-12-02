03acac8ce602e4d8d4579a24d3cb7230d9a8027f
teten <tennugraha777@gmail.com>
2021-12-02 16:29:00 +0700

    update dependency

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <Row className="row">
+          <Col xs={12}>
+            <h1>My New React Bootstrap SPA</h1>
+            <Button>Look, I'm a button!</Button>
+          </Col>
+        </Row>
+      </Container>
   );
 }
 
